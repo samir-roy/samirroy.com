86e94bf3c58905851863f9723489ca5b5e150933
Samir Roy <rsamir@gmail.com>
2021-09-23 06:58:41 -0400

    Fix pong animation for Safari 15

--- src/components/animations/PongAnimation.tsx
+++ src/components/animations/PongAnimation.tsx
@@ -3,8 +3,8 @@ import '@styles/pongAnimation.css';
 
 const PongAnimation = () => (
   <div className="pong-wrapper">
-    <div className="pong-left-paddle" />
     <div className="pong-ball" />
+    <div className="pong-left-paddle" />
     <div className="pong-right-paddle" />
     <div className="pong-clear" />
   </div>
