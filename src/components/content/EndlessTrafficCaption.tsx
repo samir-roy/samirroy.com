991f578a40d71d4be5adcfbf19c74c30cc904606
Samir Roy <rsamir@gmail.com>
2021-09-23 08:43:49 -0400

    Add CTA buttons for projects (#3)

--- src/components/content/EndlessTrafficCaption.tsx
+++ src/components/content/EndlessTrafficCaption.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react';
+import AppStoreButton from '@sprites/AppStoreButton';
+
+const styles = {
+  body: {
+    fontSize: '1.6em',
+    textAlign: 'justify',
+    fontWeight: 200,
+    marginBottom: 32,
+  } as React.CSSProperties,
+  button: {
+    width: 150,
+    margin: 'auto',
+  } as React.CSSProperties,
+};
 
 const EndlessTrafficCaption = () => (
-  <p style={{ fontSize: '1.6em', textAlign: 'justify', fontWeight: 200 }}>
-    Endless Traffic is a rush hour traffic frenzy arcade game for the iPhone and Apple Watch. Manage increasingly
-    rowdy traffic during rush hour to keep the intersection clear of accidents.
-  </p>
+  <>
+    <p style={styles.body}>
+      Endless Traffic is a rush hour traffic frenzy arcade game for the iPhone and Apple Watch. Manage increasingly
+      rowdy traffic during rush hour to keep the intersection clear of accidents.
+    </p>
+    <div>
+      <div style={styles.button}>
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        <a href="https://itunes.apple.com/app/id1226961549" className="no-hover" style={{ width: 150 }}><AppStoreButton /></a>
+      </div>
+    </div>
+  </>
 );
 
 export default React.memo(EndlessTrafficCaption);
--- src/components/content/PoonjiCaption.tsx
+++ src/components/content/PoonjiCaption.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react';
 
+const styles = {
+  body: {
+    fontSize: '1.6em',
+    textAlign: 'justify',
+    fontWeight: 200,
+    marginBottom: 32,
+  } as React.CSSProperties,
+  button: {
+    width: 150,
+    height: 50,
+    margin: 'auto',
+  } as React.CSSProperties,
+  link: {
+    minWidth: 150,
+  } as React.CSSProperties,
+};
+
 const PoonjiCaption = () => (
-  <p style={{ fontSize: '1.6em', textAlign: 'justify', fontWeight: 200 }}>
-    Poonji is a no-code platform for creating algorithmic trading bots
-    using visual tools to combine configurable building blocks.
-  </p>
+  <>
+    <p style={styles.body}>
+      Poonji is a no-code platform for creating algorithmic trading bots
+      using visual tools to combine configurable building blocks.
+    </p>
+    <div style={styles.button}>
+      <a href="https://www.poonji.io" className="button" style={styles.link}>
+        Visit Poonji
+      </a>
+    </div>
+  </>
 );
 
 export default React.memo(PoonjiCaption);
